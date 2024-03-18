a2987dc2bbe13e514529a3e3b9fed89c52a2137c
ElironB <141769879+ElironB@users.noreply.github.com>
2024-03-18 17:27:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.service import Service
 import time
 import os
 
